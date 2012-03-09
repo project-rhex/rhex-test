a3dc3c5008c9040b8d1459bc21dfa011d6fda23e
Jason Mathews <mathews@mitre.org>
2012-03-09 12:12:46 -0500

    add keepDocument property and appropriate setProperty + cleanup handling

--- src/org/mitre/hdata/test/tests/BaseUrlGetTest.java
+++ src/org/mitre/hdata/test/tests/BaseUrlGetTest.java
@@ -21,7 +21,7 @@ import java.util.List;
  * 6.2.1 GET Operation on the Base URL
  *
  * The server MUST offer an Atom 1.0 compliant feed of all child sections specified in
- * HRF specification [1], as identified in corresponding sections node in the root document.
+ * HRF specification, as identified in corresponding sections node in the root document.
  *
  * Status Code: 200, 404
  * </pre>
@@ -32,6 +32,12 @@ import java.util.List;
 public class BaseUrlGetTest extends BaseXmlTest {
 
 	private static final Logger log = LoggerFactory.getLogger(BaseUrlGetTest.class);
+	private Document doc;
+
+	private boolean keepDocument;
+
+	// property
+	public static final String PROP_KEEP_DOCUMENT_BOOL = "keepDocument";
 
 	@NonNull
 	public String getId() {
@@ -102,12 +108,19 @@ public class BaseUrlGetTest extends BaseXmlTest {
 
 		final HttpEntity entity = response.getEntity();
 		final String contentType = ClientHelper.getContentType(entity);
+		if (MIME_TEXT_HTML.equals(contentType)) {
+			throw new TestException("Expected application/atom+xml content-type but was: text/html");
+		}
 		if (!MIME_APPLICATION_ATOM_XML.equals(contentType)) {
 			addWarning("Expected application/atom+xml content-type but was: " + contentType);
 		}
-		long len = entity.getContentLength();
+		// contentType value could be text/xml or application/xml and still be valid.
+		// Return content-type is not explicitly mandated.
+
 		// minimum length expected is 43 bytes or a negative number if unknown
+		long len = entity.getContentLength();
 		assertTrue(len < 0 || len >= 43, "Expecting valid ATOM XML document for baseURL; returned length was " + len);
+
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		entity.writeTo(bos);
 		/*
@@ -120,9 +133,13 @@ public class BaseUrlGetTest extends BaseXmlTest {
 		 */
 		Document doc = getValidatingAtom(context, bos);
 		// System.out.println("XXX: xmlErrors=" + xmlErrors + " warnings=" + getWarnings().size());
-		assertTrue(xmlErrors == 0, "Content has errors in ATOM feed");
+		// assertTrue(xmlErrors == 0, "Content has errors in ATOM feed");
+		assertFalse(fatalXmlError, "Atom XML has a fatal XML error");
 		final Element root = doc.getRootElement();
 		assertEquals(NAMESPACE_W3_ATOM_2005, root.getNamespace().getURI());
+
+		// only keep copy of the DOM if the test was successful if which case dependent tests may access it
+		this.doc = doc;
 
 		setStatus(StatusEnumType.SUCCESS);
 		/*
@@ -131,4 +148,29 @@ public class BaseUrlGetTest extends BaseXmlTest {
 		 */
 	}
 
+	/**
+	 * Set property on this test.
+	 *
+	 * @param key
+	 * @param value
+	 * @exception ClassCastException if target type does not match expected type typically indicated
+	 * as ending of the property name constant (e.g. PROP_KEEP_DOCUMENT_BOOL)
+	 */
+	public void setProperty(String key, Object value) {
+		// System.out.printf("XXX: setProperty %s: %s%n", key, value);
+		if (PROP_KEEP_DOCUMENT_BOOL.equals(key))
+			keepDocument = (Boolean)value;
+		else super.setProperty(key, value);
+	}
+
+	public void cleanup() {
+		super.cleanup();
+		if (!keepDocument) {
+			doc = null;
+		}
+	}
+
+	public Document getDocument() {
+		return doc;
+	}
 }
