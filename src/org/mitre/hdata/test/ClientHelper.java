131b0c4a793bd473def017171281388fb1dca274
Jason Mathews <mathews@mitre.org>
2012-03-16 15:35:28 -0400

    update javadoc

--- src/org/mitre/hdata/test/ClientHelper.java
+++ src/org/mitre/hdata/test/ClientHelper.java
@@ -14,6 +14,14 @@ import org.apache.http.conn.params.ConnRoutePNames;
  */
 public final class ClientHelper {
 
+	/**
+	 * Return content-type value with optional character encoding
+	 * stripped off.
+	 *
+	 * @param entity HttpEntity returned from HttpResponse
+	 * @return content-type from HTTP response header, null if not present
+	 */
+	@CheckForNull
 	public static String getContentType(HttpEntity entity) {
 		return getContentType(entity, false);
 	}
@@ -21,10 +29,12 @@ public final class ClientHelper {
 	/**
 	 * Return content-type value and optionally strip off the character encoding
 	 * following the content MIME type.
+	 *
 	 * @param entity HttpEntity returned from HttpResponse
 	 * @param includeEncoding Flag if true it include character encoding if provided
 	 *                        otherwise strip encoding from the content type
 	 * @return content-type from HTTP response header, null if not present
+	 * @see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.17
 	 */
 	@CheckForNull
 	public static String getContentType(HttpEntity entity, boolean includeEncoding) {
