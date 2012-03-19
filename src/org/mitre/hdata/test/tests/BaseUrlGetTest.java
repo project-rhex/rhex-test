8f806ce61b11c08afd03b2b260a77235d2cc5ee1
Jason Mathews <mathews@mitre.org>
2012-03-19 16:50:38 -0400

    moved setProperty() + cleanup() + doc field + keepDocument field to parent class BaseXmlTest

--- src/org/mitre/hdata/test/tests/BaseUrlGetTest.java
+++ src/org/mitre/hdata/test/tests/BaseUrlGetTest.java
@@ -3,6 +3,7 @@ package org.mitre.hdata.test.tests;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.jdom.Document;
@@ -32,12 +33,6 @@ import java.util.List;
 public class BaseUrlGetTest extends BaseXmlTest {
 
 	private static final Logger log = LoggerFactory.getLogger(BaseUrlGetTest.class);
-	private Document doc;
-
-	private boolean keepDocument;
-
-	// property
-	public static final String PROP_KEEP_DOCUMENT_BOOL = "keepDocument";
 
 	@NonNull
 	public String getId() {
@@ -82,7 +77,7 @@ public class BaseUrlGetTest extends BaseXmlTest {
 					System.out.println("\t" + header.getName() + ": " + header.getValue());
 				}
 			}
-			response = client.execute(req);
+			HttpResponse response = client.execute(req);
 			int code = response.getStatusLine().getStatusCode();
 			if (log.isDebugEnabled()) {
 				System.out.println("Response status=" + code);
@@ -90,7 +85,7 @@ public class BaseUrlGetTest extends BaseXmlTest {
 					System.out.println("\t" + header.getName() + ": " + header.getValue());
 				}
 			}
-			validateContent(code, context);
+			validateContent(code, context, response);
 		} catch (JDOMException e) {
 			throw new TestException(e);
 		} catch (IOException e) {
@@ -100,7 +95,9 @@ public class BaseUrlGetTest extends BaseXmlTest {
 		}
 	}
 
-	protected void validateContent(int code, Context context) throws TestException, IOException, JDOMException {
+	protected void validateContent(int code, Context context, HttpResponse response)
+			throws TestException, IOException, JDOMException {
+
 		if (code != 200) {
 			setStatus(StatusEnumType.FAILED, "Unexpected HTTP response: " + code);
 			return;
@@ -138,8 +135,9 @@ public class BaseUrlGetTest extends BaseXmlTest {
 		final Element root = doc.getRootElement();
 		assertEquals(NAMESPACE_W3_ATOM_2005, root.getNamespace().getURI());
 
-		// only keep copy of the DOM if the test was successful if which case dependent tests may access it
-		this.doc = doc;
+		// only keep copy of the DOM and response if the test was successful if which case dependent tests may access it
+		setDocument(doc);
+		setResponse(response);
 
 		setStatus(StatusEnumType.SUCCESS);
 		/*
@@ -148,29 +146,4 @@ public class BaseUrlGetTest extends BaseXmlTest {
 		 */
 	}
 
-	/**
-	 * Set property on this test.
-	 *
-	 * @param key
-	 * @param value
-	 * @exception ClassCastException if target type does not match expected type typically indicated
-	 * as ending of the property name constant (e.g. PROP_KEEP_DOCUMENT_BOOL)
-	 */
-	public void setProperty(String key, Object value) {
-		// System.out.printf("XXX: setProperty %s: %s%n", key, value);
-		if (PROP_KEEP_DOCUMENT_BOOL.equals(key))
-			keepDocument = (Boolean)value;
-		else super.setProperty(key, value);
-	}
-
-	public void cleanup() {
-		super.cleanup();
-		if (!keepDocument) {
-			doc = null;
-		}
-	}
-
-	public Document getDocument() {
-		return doc;
-	}
 }
--- src/org/mitre/hdata/test/tests/BaseUrlRootXml.java
+++ src/org/mitre/hdata/test/tests/BaseUrlRootXml.java
@@ -4,6 +4,7 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.jdom.Document;
@@ -40,15 +41,10 @@ import java.util.List;
 public class BaseUrlRootXml extends BaseXmlTest {
 
 	private static final Logger log = LoggerFactory.getLogger(BaseUrlRootXml.class);
-	private Document doc;
-	private boolean keepDocument;
-
-	// property
-	public static final String PROP_KEEP_DOCUMENT_BOOL = "keepDocument";
 
 	@NonNull
 	public String getId() {
-		return "6.3.1";
+		return "6.3.1.1";
 	}
 
 	@Override
@@ -64,25 +60,6 @@ public class BaseUrlRootXml extends BaseXmlTest {
 	@NonNull
 	public List<Class<? extends TestUnit>> getDependencyClasses() {
 		return Collections.emptyList(); // none
-	}
-
-	public Document getDocument() {
-		return doc;
-	}
-
-	/**
-	 * Set property on this test.
-	 *
-	 * @param key
-	 * @param value
-	 * @exception ClassCastException if target type does not match expected type typically indicated
-	 * as ending of the property name constant (e.g. PROP_KEEP_DOCUMENT_BOOL)
-	 */
-	public void setProperty(String key, Object value) {
-		// System.out.printf("XXX: setProperty %s: %s%n", key, value);
-		if (PROP_KEEP_DOCUMENT_BOOL.equals(key))
-			keepDocument = (Boolean)value;
-		else super.setProperty(key, value);
 	}
 
 	public void execute() throws TestException {
@@ -101,15 +78,8 @@ public class BaseUrlRootXml extends BaseXmlTest {
 					System.out.println("\t" + header.getName() + ": " + header.getValue());
 				}
 			}
-			response = client.execute(req);
-			int code = response.getStatusLine().getStatusCode();
-			if (log.isDebugEnabled()) {
-				System.out.println("Response status=" + code);
-				for (Header header : response.getAllHeaders()) {
-					System.out.println("\t" + header.getName() + ": " + header.getValue());
-				}
-			}
-			validateContent(code, context);
+			HttpResponse response = client.execute(req);
+			validateContent(context, response);
 		} catch (JDOMException e) {
 			throw new TestException(e);
 		} catch (IOException e) {
@@ -122,20 +92,29 @@ public class BaseUrlRootXml extends BaseXmlTest {
 		// System.out.println();
 	}
 
-	protected void validateContent(int code, Context context) throws TestException, IOException, JDOMException {
+	protected void validateContent(Context context, HttpResponse response)
+			throws TestException, IOException, JDOMException {
+
+		int code = response.getStatusLine().getStatusCode();
+		if (log.isDebugEnabled()) {
+			System.out.println("Response status=" + code);
+			for (Header header : response.getAllHeaders()) {
+				System.out.println("\t" + header.getName() + ": " + header.getValue());
+			}
+		}
 		if (code != 200) {
 			setStatus(StatusEnumType.FAILED, "Unexpected HTTP response: " + code);
 			return;
 		}
 		final HttpEntity entity = response.getEntity();
+		if (entity == null) {
+			setStatus(StatusEnumType.FAILED, "Expect XML in body of response");
+			return;
+		}
 		final String contentType = ClientHelper.getContentType(entity, false);
 		// content-type = text/xml OR application/xml
 		if (!MIME_TEXT_XML.equals(contentType) && !MIME_APPLICATION_XML.equals(contentType)) {
 			addWarning("Expected text/xml content-type but was: " + contentType);
-		}
-		if (entity == null) {
-			setStatus(StatusEnumType.FAILED, "Expect XML in body of response");
-			return;
 		}
 		long len = entity.getContentLength();
 		// minimum length expected is 66 bytes or a negative number if unknown
@@ -167,9 +146,11 @@ public class BaseUrlRootXml extends BaseXmlTest {
 
 		checkRootDocument(root);
 
-		// only keep copy of the DOM if the test was successful if which case dependent tests may access it
-		this.doc = doc;
-
+		// only keep copy of the DOM and response if the test was successful if which case dependent tests may access it
+		setResponse(response);
+		if (keepDocument) {
+			setDocument(doc);
+		}
 		setStatus(StatusEnumType.SUCCESS);
 	}
 
@@ -178,7 +159,7 @@ public class BaseUrlRootXml extends BaseXmlTest {
 		Element extensionsElt = root.getChild("extensions", ns);
 		HashSet<String> extensions = new HashSet<String>();
 		if (extensionsElt != null) {
-			// verify HL7 V1.0 2.2 bullet item #8: /hrf:extensions/hrf:extension/@extensionId (xs:string, 1)
+			// verify HL7v3 HRF 1.0 - 2.2 bullet item #8: /hrf:extensions/hrf:extension/@extensionId (xs:string, 1)
 			// This attribute contains a local identifier for the extension. It MUST be unique within the root document.
 			for(Object child : extensionsElt.getChildren("extension", ns)) {
 				if (!(child instanceof Element)) continue;
@@ -192,7 +173,7 @@ public class BaseUrlRootXml extends BaseXmlTest {
 		}
 		Element sections = root.getChild("sections", ns);
 		if (sections != null) {
-			// verify HL7 V1.0 2.2 bullet item #12:	/htf:sections/hrf:section/@extensionId (xs:string, 1)
+			// verify HL7v3 HRF 1.0 - 2.2 bullet item #12: /htf:sections/hrf:section/@extensionId (xs:string, 1)
 			// This identifier MUST be equal to the identifier of any of the registered extension elements,
 			// as identified by the id attribute of the <extension> element.
 			for(Object child : sections.getChildren("section", ns)) {
@@ -204,13 +185,6 @@ public class BaseUrlRootXml extends BaseXmlTest {
 					break;
 				}
 			}
-		}
-	}
-
-	public void cleanup() {
-		super.cleanup();
-		if (!keepDocument) {
-			doc = null;
 		}
 	}
 
